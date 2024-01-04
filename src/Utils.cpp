b0b7f73e4a442e7a53103dccad21aadaf465dfa1
YoussefAssem <yo_90_32@hotmail.com>
2024-01-04 14:58:22 +0100

    Added reserving for whole points vector. Removed reserving of smaller portions.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -34,21 +34,30 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
     std::vector<Point> points;
 
     tinygltf::Model model;
-    LoadModel(model, scene.data());
+    assert(LoadModel(model, scene.data()));
 
-    auto matrix = model.nodes[0].matrix;
+    auto& matrix = model.nodes[0].matrix;
     glm::mat4 modelMatrix(
         matrix[0],  matrix[1],  matrix[2],  matrix[3], 
         matrix[4],  matrix[5],  matrix[6],  matrix[7], 
         matrix[8],  matrix[9],  matrix[10], matrix[11], 
         matrix[12], matrix[13], matrix[14], matrix[15]);
 
+    uint32_t totalPointCount = 0;
     for (auto& mesh : model.meshes) {
         for (auto& primitive : mesh.primitives) {
-            size_t currentPointsCount = points.size();
+            for (auto& attribute : primitive.attributes) {
+                totalPointCount += model.accessors[attribute.second].count;
+                break;
+            }
+        }
+    }
+    points.resize(totalPointCount);
 
-            for (auto& attribute : primitive.attributes) 
-            {
+    uint32_t currentPointCount = 0;
+    for (auto& mesh : model.meshes) {
+        for (auto& primitive : mesh.primitives) {
+            for (auto& attribute : primitive.attributes) {
                 if (attribute.first.compare("POSITION") == 0) {
                     auto& accessor = model.accessors[attribute.second];
 
@@ -62,16 +71,11 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
                     uint32_t bufferOffset = accessor.byteOffset + bufferView.byteOffset;
                     uint32_t stride = accessor.ByteStride(bufferView);
 
-                    //resize points if we have not
-                    if (currentPointsCount == points.size()) {
-                        points.resize(currentPointsCount + primPointCount);
-                    }
-
                     for (size_t i = 0; i < primPointCount; i++) {
                         glm::vec4 fColor = glm::vec4(0,0,0,1);
                         memcpy(&fColor, &buffer[bufferOffset + i * stride], stride);
 
-                        points[currentPointsCount + i].position = modelMatrix * fColor;
+                        points[currentPointCount + i].position = modelMatrix * fColor;
                     }
                 }
 
@@ -88,22 +92,21 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
                     uint32_t bufferOffset = accessor.byteOffset + bufferView.byteOffset;
                     uint32_t stride = accessor.ByteStride(bufferView);
 
-                    // resize points if we have not
-                    if (currentPointsCount == points.size()) {
-                        points.resize(currentPointsCount + pointCount);
-                    }
-
                     for (size_t i = 0; i < pointCount; i++) {
                         glm::vec4 fColor;
                         memcpy(&fColor, &buffer[bufferOffset + i * stride], stride);
-
-                        points[currentPointsCount + i].r = fColor.r * 255;
-                        points[currentPointsCount + i].g = fColor.g * 255;
-                        points[currentPointsCount + i].b = fColor.b * 255;
-                        points[currentPointsCount + i].a = 255;
+                        
+                        points[currentPointCount + i].r = fColor.r * 255;
+                        points[currentPointCount + i].g = fColor.g * 255;
+                        points[currentPointCount + i].b = fColor.b * 255;
+                        points[currentPointCount + i].a = 255;
                     }
                 }
             }
+
+            //get count from first attribute
+            auto& attribute = primitive.attributes.begin()->second;
+            currentPointCount += model.accessors[attribute].count;
         }
     }
 
