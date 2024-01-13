d173776070d640482fc8307f37b91b2d4a6ef4b2
chrylt <chris.kobalt@tum.de>
2024-01-13 12:46:40 +0100

    ops, header file now also commited

--- src/TGAPointCloudAcceleration.h
+++ src/TGAPointCloudAcceleration.h
@@ -3,6 +3,7 @@
 #include <tga/tga.hpp>
 #include <tga/tga_utils.hpp>
 #include <tga/tga_vulkan/tga_vulkan.hpp>
+#include "Utils.h"
 
 class TGAPointCloudAcceleration final {
 public:
@@ -24,6 +25,13 @@ public:
     ~TGAPointCloudAcceleration();
 
 private:
+    struct BatchesCompressed {
+        std::vector<CompressedPosition> lowPrecisions;
+        std::vector<CompressedPosition> mediumPrecisions;
+        std::vector<CompressedPosition> highPrecisions;
+        std::vector<CompressedColor> colors;
+    };
+
     tga::Interface &backend_;
 
     std::uint32_t batchCount_;
@@ -31,4 +39,6 @@ private:
     PointBuffers pointsBufferPack_;
     tga::Buffer accelerationBuffer_;
     tga::Buffer batchesBuffer_;
+
+    BatchesCompressed ConvertToAdaptivePrecision(const std::vector<Batch> batches) const;
 };
