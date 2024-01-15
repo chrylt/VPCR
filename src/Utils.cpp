24763a5d684ce4df9b634809c4382c55565d77cd
YoussefAssem <yo_90_32@hotmail.com>
2024-01-15 18:34:08 +0100

    Adjusted format of changed files

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,11 +2,12 @@
 
 #define TINYGLTF_IMPLEMENTATION
 #include <tinygltf/tiny_gltf.h>
+
 #include <iostream>
 
 namespace
 {
-//Throws exception if model was not loaded. Either no file or faulty gltf.
+// Throws exception if model was not loaded. Either no file or faulty gltf.
 auto LoadModel(const std::string_view filename)
 {
     tinygltf::Model model;
@@ -40,10 +41,9 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
     auto modelMatrix = glm::mat4(1.0);
     if (!model.nodes.empty()) {
         const auto& matrix = model.nodes[0].matrix;
-        modelMatrix = glm::mat4(matrix[ 0], matrix[ 1], matrix[ 2], matrix[ 3], 
-                                matrix[ 4], matrix[ 5], matrix[ 6], matrix[ 7],
-                                matrix[ 8], matrix[ 9], matrix[10], matrix[11], 
-                                matrix[12], matrix[13], matrix[14], matrix[15]);
+        modelMatrix =
+            glm::mat4(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5], matrix[6], matrix[7], matrix[8],
+                      matrix[9], matrix[10], matrix[11], matrix[12], matrix[13], matrix[14], matrix[15]);
     }
 
     std::size_t totalPointCount = 0;
@@ -75,7 +75,7 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
 
                 if (accessorName.compare("POSITION") == 0) {
                     for (std::uint32_t i = 0; i < primPointCount; ++i) {
-                        glm::vec4 position = glm::vec4(0,0,0,1);
+                        glm::vec4 position = glm::vec4(0, 0, 0, 1);
                         memcpy(&position, &buffer[bufferOffset + i * stride], stride);
 
                         points[currentPointCount + i].position = modelMatrix * position;
@@ -84,7 +84,7 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
                     for (std::uint32_t i = 0; i < primPointCount; ++i) {
                         glm::vec4 fColor;
                         memcpy(&fColor, &buffer[bufferOffset + i * stride], stride);
-                        
+
                         points[currentPointCount + i].r = static_cast<std::uint8_t>(fColor.r * 255);
                         points[currentPointCount + i].g = static_cast<std::uint8_t>(fColor.g * 255);
                         points[currentPointCount + i].b = static_cast<std::uint8_t>(fColor.b * 255);
@@ -93,7 +93,7 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
                 }
             }
 
-            //get count from first attribute
+            // get count from first attribute
             if (!primitive.attributes.empty()) {
                 const auto& attribute = primitive.attributes.begin()->second;
                 currentPointCount += model.accessors[attribute].count;
@@ -104,15 +104,14 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
     return points;
 }
 
-
 }  // namespace
 
 std::vector<Batch> LoadScene(std::string_view scene, std::vector<Point>& points)
 {
     points = LoadScenePoints(scene);
 
-    //TODO add a hashmap for morton code to make this faster or 
-    //Implement this https://ieeexplore.ieee.org/document/5383353
+    // TODO add a hashmap for morton code to make this faster or
+    // Implement this https://ieeexplore.ieee.org/document/5383353
     std::sort(points.begin(), points.end());
 
     constexpr auto maxBatchSize = 4;
@@ -120,7 +119,8 @@ std::vector<Batch> LoadScene(std::string_view scene, std::vector<Point>& points)
     return batch.Subdivide(maxBatchSize);
 }
 
-std::uint64_t Point::mortonIndex() const {
+std::uint64_t Point::mortonIndex() const
+{
     auto ix = reinterpret_cast<const std::uint32_t&>(position.x);
     auto iy = reinterpret_cast<const std::uint32_t&>(position.y);
     auto iz = reinterpret_cast<const std::uint32_t&>(position.z);
@@ -195,8 +195,7 @@ std::vector<Batch> Batch::Subdivide(const std::uint32_t maxBatchCount)
     if (points.size() < maxBatchCount) {
         leaf = true;
         result.push_back(*this);
-    } 
-    else {
+    } else {
         std::array<Batch, 8> children;
         for (size_t i = 0; i < children.size(); i++) {
             children[i] = Batch(iteration + 1, i << (54U - iteration * 3U) | mortonCode, points.subspan(0, 0));
@@ -204,7 +203,7 @@ std::vector<Batch> Batch::Subdivide(const std::uint32_t maxBatchCount)
 
         // splitting points into octree nodes
         auto prevNodeStartIT = points.begin();
-        auto prevPointNodeID = MortonToNodeID(points.begin()->mortonIndex());                 
+        auto prevPointNodeID = MortonToNodeID(points.begin()->mortonIndex());
         for (auto it = points.begin(); it != points.end(); ++it) {
             const auto currPointNodeID = MortonToNodeID(it->mortonIndex());
 
@@ -222,7 +221,7 @@ std::vector<Batch> Batch::Subdivide(const std::uint32_t maxBatchCount)
             }
         }
 
-        //filter subdivide results
+        // filter subdivide results
         for (auto i = 0; i < children.size(); i++) {
             auto res = children[i].Subdivide(maxBatchCount);
 
@@ -240,6 +239,7 @@ std::vector<Batch> Batch::Subdivide(const std::uint32_t maxBatchCount)
     return result;
 }
 
-const std::uint32_t Batch::MortonToNodeID(const std::uint64_t mortonCode) const {
+const std::uint32_t Batch::MortonToNodeID(const std::uint64_t mortonCode) const
+{
     return (mortonCode >> (60U - iteration * 3U)) & 0x7U;
 }
