43e402ae69b31f5b10514e49de42a1e282b4ffcd
Sebastian Merk <33519687+Atzubi@users.noreply.github.com>
2024-02-04 19:10:51 +0100

    Disable gui options when not having any effect (#35)
    * Disable gui options when not having any effect
    
    * Fix fov disable and aa disables
    
    * Increase far plane distance, fix fixes
    
    * fixed OPDAA for new near-far-plane-distance (again)
    
    * Add camera speed slider
    
    ---------
    
    Co-authored-by: Sebastian Merk <lolmaunz@gmail.com>
    Co-authored-by: chrylt <chris.kobalt@tum.de>

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -9,18 +9,17 @@
 void RenderGui(const Config& config)
 {
     ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(ImVec2(0, 0));  // Automatic size
     ImGui::SetNextWindowBgAlpha(0.7f);
     ImGui::Begin("State", nullptr,
                  ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoTitleBar);
     ImGui::Text("Frame rate: %s FPS", config.Get<std::string>("TitleBar.fps").value().c_str());
     ImGui::Text("Batches   : %s/%s", config.Get<std::string>("TitleBar.DrawnBatches").value().c_str(),
                 config.Get<std::string>("TitleBar.TotalBatches").value().c_str());
-    ImGui::Text("Anti-Aliasing-Mode: %s", config.Get<std::string>("TitleBar.AAMode").value().c_str());
-    ImGui::Text("Warp-Wide-Deduplication-Mode: %s", config.Get<std::string>("TitleBar.dedMode").value().c_str());
 
     ImGui::End();
 
-    constexpr std::uint32_t settingsWidth = 260;
+    constexpr std::uint32_t settingsWidth = 285;
     const auto res = config.Get<std::vector<std::uint32_t>>("resolution").value();
     ImGui::SetNextWindowPos(ImVec2(static_cast<float>(res[0]) - settingsWidth, 0));
     ImGui::SetNextWindowSize(ImVec2(settingsWidth, static_cast<float>(res[1])));
@@ -32,6 +31,8 @@ void RenderGui(const Config& config)
         auto colorBatch = config.Get<bool>("LOD.colorBatch");
         if (ImGui::Checkbox("Color by Batch", &colorBatch.value())) {
             config.SetDirty("LOD.colorBatch", colorBatch.value());
+            config.SetDirty("LOD.colorDepth", false);
+            config.SetDirty("DAA.visualizeDensityBuckets", false);
         }
         if (ImGui::IsItemHovered()) {
             ImGui::SetTooltip("Give each batch a unique color.");
@@ -64,6 +65,7 @@ void RenderGui(const Config& config)
             ImGui::SetTooltip("Enables frustum culling which uses spheres which are set around batches.");
         }
 
+        ImGui::BeginDisabled(!cullingEnabled.value());
         auto currentCullingFov = config.Get<float>("LOD.cullingFov");
         const auto maxCullingFov = config.Get<float>("LOD.defaultCullingFov");
         ImGui::Text("Culling FOV");
@@ -78,6 +80,7 @@ void RenderGui(const Config& config)
         if (ImGui::Button("Reset Culling FOV")) {
             config.SetDirty("LOD.cullingFov", config.Get<float>("LOD.defaultCullingFov").value());
         }
+        ImGui::EndDisabled();
 
         ImGui::TreePop();
     }
@@ -98,6 +101,8 @@ void RenderGui(const Config& config)
         auto colorDepth = config.Get<bool>("LOD.colorDepth");
         if (ImGui::Checkbox("Color by Depth", &colorDepth.value())) {
             config.SetDirty("LOD.colorDepth", colorDepth.value());
+            config.SetDirty("LOD.colorBatch", false);
+            config.SetDirty("DAA.visualizeDensityBuckets", false);
         }
         if (ImGui::IsItemHovered()) {
             ImGui::SetTooltip("Give each lod depth a unique color.");
@@ -114,6 +119,7 @@ void RenderGui(const Config& config)
             config.SetDirty("LOD.level", currentTreeLayer.value());
         }
 
+        ImGui::BeginDisabled(!lodEnabled.value());
         auto currentLODSelection = config.Get<float>("LOD.selection");
         const auto maxLODSelection = config.Get<float>("LOD.maxSelection");
         ImGui::Text("LOD Selection Criteria");
@@ -129,6 +135,7 @@ void RenderGui(const Config& config)
         if (ImGui::Button("Reset selection")) {
             config.SetDirty("LOD.selection", config.Get<float>("LOD.defaultSelection").value());
         }
+        ImGui::EndDisabled();
 
         ImGui::TreePop();
     }
@@ -137,6 +144,7 @@ void RenderGui(const Config& config)
     ImGui::Separator();
     ImGui::Spacing();
 
+    ImGui::BeginDisabled(config.Get<AntiAliasingMode>("AA.currAAMode").value() != AntiAliasingMode::Off);
     if (ImGui::TreeNodeEx("Warp Wide Deduplication", ImGuiTreeNodeFlags_DefaultOpen)) {
         auto currentWarpWideMode = config.Get<int>("LOD.warpWideDeduplication");
         if (ImGui::RadioButton("None", &currentWarpWideMode.value(), 0)) {
@@ -164,6 +172,7 @@ void RenderGui(const Config& config)
 
         ImGui::TreePop();
     }
+    ImGui::EndDisabled();
 
     ImGui::Spacing();
     ImGui::Separator();
@@ -176,6 +185,7 @@ void RenderGui(const Config& config)
             config.SetDirty("AA.currAAMode", static_cast<AntiAliasingMode>(currMode));
         }
 
+        ImGui::BeginDisabled(currMode == AntiAliasingMode::Off);
         auto errorShow = config.Get<bool>("AA.errorShow");
         if (ImGui::Checkbox("Show Error Color", &errorShow.value())) {
             config.SetDirty("AA.errorShow", errorShow.value());
@@ -188,15 +198,9 @@ void RenderGui(const Config& config)
                 "\n(OPDAA) Green: Linked List Insert Timeout"
                 "\n(OPDAA) Blue: Filled Bucket Count at Limit");
         }
+        ImGui::EndDisabled();
 
-        auto preventedOverflowVis = config.Get<bool>("AA.preventedOverflowVis");
-        if (ImGui::Checkbox("Color Accumulation Overflow", &preventedOverflowVis.value())) {
-            config.SetDirty("AA.preventedOverflowVis", preventedOverflowVis.value());
-        }
-        if (ImGui::IsItemHovered()) {
-            ImGui::SetTooltip("Visualizes accumulation overflow prevention.");
-        }
-
+        ImGui::BeginDisabled(currMode != AntiAliasingMode::TwoPass);
         auto depthPerc = config.Get<float>("TPAA.depthPerc");
         ImGui::Text("(TPAA) Depth Percentage");
         if (ImGui::IsItemHovered()) {
@@ -206,37 +210,60 @@ void RenderGui(const Config& config)
                                ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_Logarithmic)) {
             config.SetDirty("TPAA.depthPerc", depthPerc.value());
         }
+        ImGui::EndDisabled();
 
         if (ImGui::TreeNodeEx("Density-Based Methods", ImGuiTreeNodeFlags_DefaultOpen)) {
+            ImGui::BeginDisabled((currMode != AntiAliasingMode::DensityOnePass) &&
+                                 (currMode != AntiAliasingMode::DensityTwoPass));
             auto preventOverflow = config.Get<bool>("AA.preventOverflow");
-            if (ImGui::Checkbox("Enable Overflow Prevention of Accumulation", &preventOverflow.value())) {
+            if (ImGui::Checkbox("Enable Overflow Prevention\nof Accumulation", &preventOverflow.value())) {
                 config.SetDirty("AA.preventOverflow", preventOverflow.value());
             }
             if (ImGui::IsItemHovered()) {
                 ImGui::SetTooltip("Toggles accumulation overflow prevention by discarding buckets.");
             }
+            ImGui::EndDisabled();
 
+            ImGui::BeginDisabled(!preventOverflow.value() || ((currMode != AntiAliasingMode::DensityOnePass) &&
+                                                              (currMode != AntiAliasingMode::DensityTwoPass)));
+            auto preventedOverflowVis = config.Get<bool>("AA.preventedOverflowVis");
+            if (ImGui::Checkbox("Color Accumulation Overflow\nPrevented", &preventedOverflowVis.value())) {
+                config.SetDirty("AA.preventedOverflowVis", preventedOverflowVis.value());
+            }
+            if (ImGui::IsItemHovered()) {
+                ImGui::SetTooltip("Visualizes accumulation overflow prevention.");
+            }
+            ImGui::EndDisabled();
+
+            ImGui::BeginDisabled((currMode != AntiAliasingMode::DensityOnePass) &&
+                                 (currMode != AntiAliasingMode::DensityTwoPass));
             auto visualizeDensityBuckets = config.Get<bool>("DAA.visualizeDensityBuckets");
             if (ImGui::Checkbox("Visualize Histogram Buckets", &visualizeDensityBuckets.value())) {
                 config.SetDirty("DAA.visualizeDensityBuckets", visualizeDensityBuckets.value());
+                config.SetDirty("LOD.colorBatch", false);
+                config.SetDirty("LOD.colorDepth", false);
             }
             if (ImGui::IsItemHovered()) {
                 ImGui::SetTooltip(
                     "Visualizes buckets of the histogram.\n(OPDAA) Randomly assigns color to bucketID\n(TPDAA) Only "
                     "renders points in bucket specified by bucketID below.");
             }
+            ImGui::EndDisabled();
 
+            ImGui::BeginDisabled(currMode != AntiAliasingMode::DensityOnePass);
             auto bucketSize = config.Get<float>("OPDAA.bucketSize");
             ImGui::Text("(OPDAA) Set Bucket Size");
             if (ImGui::IsItemHovered()) {
                 ImGui::SetTooltip(
                     "Set the world space bucket size for the one-pass density-based anti-aliasing approach.");
             }
-            if (ImGui::SliderFloat("##(OPDAA) Set Bucket Size", &bucketSize.value(), 0.0000001, 0.01, "%.10f",
+            if (ImGui::SliderFloat("##(OPDAA) Set Bucket Size", &bucketSize.value(), 0.00001f, 20.f, "%.10f",
                                    ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_Logarithmic)) {
                 config.SetDirty("OPDAA.bucketSize", bucketSize.value());
             }
+            ImGui::EndDisabled();
 
+            ImGui::BeginDisabled(!visualizeDensityBuckets.value() || (currMode != AntiAliasingMode::DensityTwoPass));
             auto bucketIDToVis = config.Get<int>("TPDAA.bucketIDToShow");
             ImGui::Text("(TPDAA) Visualize by BucketID");
             if (ImGui::IsItemHovered()) {
@@ -246,8 +273,20 @@ void RenderGui(const Config& config)
                                  ImGuiSliderFlags_AlwaysClamp)) {
                 config.SetDirty("TPDAA.bucketIDToShow", bucketIDToVis.value());
             }
+            ImGui::EndDisabled();
 
             ImGui::TreePop();
+        }
+
+        ImGui::TreePop();
+    }
+
+    if (ImGui::TreeNodeEx("Camera", ImGuiTreeNodeFlags_DefaultOpen)) {
+        auto camSpeed = config.Get<float>("camera.moveSpeed");
+        ImGui::Text("Move Speed");
+        if (ImGui::DragFloat("##Move Speed", &camSpeed.value(), 1.f, 0.f, 10000.f, "%.3f",
+                             ImGuiSliderFlags_Logarithmic | ImGuiSliderFlags_AlwaysClamp)) {
+            config.SetDirty("camera.moveSpeed", camSpeed.value());
         }
 
         ImGui::TreePop();
