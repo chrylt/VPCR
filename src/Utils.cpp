c1d12e207f81b6aaac284f1dccce955ab889cad9
Sebastian Merk <lolmaunz@gmail.com>
2024-02-04 15:08:35 +0100

    Use empty instead

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -43,7 +43,7 @@ std::vector<Point> LoadScenePoints(const std::string_view scene)
     auto modelMatrix = glm::mat4(1.0);
     if (!model.nodes.empty()) {
         const auto& matrix = model.nodes[0].matrix;
-        if (matrix.size()) {
+        if (!matrix.empty()) {
             modelMatrix =
                 glm::mat4(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5], matrix[6], matrix[7],
                           matrix[8], matrix[9], matrix[10], matrix[11], matrix[12], matrix[13], matrix[14], matrix[15]);
